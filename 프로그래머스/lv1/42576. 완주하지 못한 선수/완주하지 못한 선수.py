f872eae262b8942833d9a0cb691d149d3dc44610
이정호 <101508190+jungho1209@users.noreply.github.com>
2023-08-11 09:56:04 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.76 ms, Memory: 26.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,10 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+            break
+            
+    return participant[-1] # 마지막 Value
