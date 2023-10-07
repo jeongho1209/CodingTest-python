34e5cd3e866dbb9ec5f4037bd3ed18fd97060cb2
이정호 <101508190+jeongho1209@users.noreply.github.com>
2023-10-07 15:28:00 +0900

    [Silver II] Title: K번째 소수, Time: 1176 ms, Memory: 289188 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15965. K번째 소수/K번째 소수.py
+++ 백준/Silver/15965. K번째 소수/K번째 소수.py
@@ -1,9 +1,9 @@
 import math
 import sys
 
-arr = [1, 1] + [0] * 5000000
+arr = [1, 1] + [0] * 8000000
 
-Max = math.sqrt(5000000)
+Max = math.sqrt(8000000)
 
 prime = []
 
@@ -11,7 +11,7 @@ N = int(sys.stdin.readline().rstrip())
 
 for i in range(2, int(Max + 1)):
     if arr[i] == 0:
-        for j in range(i * i, 5000000, i):
+        for j in range(i * i, 8000000, i):
             arr[j] = 1
 
 for i in range(len(arr)):
