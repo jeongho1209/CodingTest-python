8ba1b880855957dd8dbe80621337fec4a2cb3bd8
이정호 <101508190+jeongho1209@users.noreply.github.com>
2023-11-09 23:15:54 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 1484 ms, Memory: 87796 KB -BaekjoonHub

---
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -0,0 +1,42 @@
+import sys
+sys.setrecursionlimit(10 ** 7)
+
+N, M = map(int, sys.stdin.readline().rstrip().split())
+
+graph = []
+
+visited = [[False] * M for _ in range(N)]
+
+answer = 0
+
+for _ in range(N):
+    graph.append(list(sys.stdin.readline().rstrip()))
+
+
+def dfs(i, j):
+    if N <= i or M <= j or i < 0 or j < 0 or graph[i][j] == 'X':
+        return 0
+
+    global answer
+
+    if graph[i][j] == 'P':
+        answer += 1
+
+    if not visited[i][j]:
+        visited[i][j] = True
+        graph[i][j] = 'S'
+        dfs(i + 1, j)
+        dfs(i, j + 1)
+        dfs(i - 1, j)
+        dfs(i, j - 1)
+
+
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 'I':
+            dfs(i, j)
+
+if answer == 0:
+    print('TT')
+else:
+    print(answer)
