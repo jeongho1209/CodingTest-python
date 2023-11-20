23c662bbf193e9bb000d74edb3a9b04bee8384bb
이정호 <101508190+jeongho1209@users.noreply.github.com>
2023-11-20 23:29:17 +0900

    [Bronze III] Title: 크림빵, Time: 48 ms, Memory: 31252 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/28214. 크림빵/크림빵.py
+++ 백준/Bronze/28214. 크림빵/크림빵.py
@@ -6,8 +6,8 @@ bread = list(map(int, sys.stdin.readline().rstrip().split()))
 ans = 0
 
 for b in range(0, len(bread), K):
-    cnt = sum(bread[b: b + K])  # 크림빵 개수
-    if cnt >= P:  # 크림빵 개수가 P개 이상 -> 판매 가능
+    cnt = K - sum(bread[b: b + K])  # 크림빵 없는 것 개수
+    if cnt < P:  # 크림빵 없는 것 개수가 P개 미만 -> 판매 가능
         ans += 1
 
 print(ans)
