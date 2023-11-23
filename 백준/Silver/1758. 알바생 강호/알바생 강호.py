2dc4c5c74d07673af37f95c4e0f328ab9c600839
이정호 <101508190+jeongho1209@users.noreply.github.com>
2023-11-23 14:21:50 +0900

    [Silver IV] Title: 알바생 강호, Time: 108 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Silver/1758. 알바생 강호/알바생 강호.py
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.py
@@ -7,8 +7,6 @@ wait_queue = []
 for _ in range(N):
     wait_queue.append(int(sys.stdin.readline().rstrip()))
 
-# 고려할 것 -> 우선 내림차순 정렬해서 가장 많이 받을 수 있게 하는데 음수 안 나오게 하는것도 고려해야함
-
 wait_queue.sort(reverse=True)
 
 ans = 0
