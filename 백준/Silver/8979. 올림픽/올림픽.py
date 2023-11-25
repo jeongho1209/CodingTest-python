17e9d733b81604b45d282da6888bb42b26b2d3b8
이정호 <101508190+jeongho1209@users.noreply.github.com>
2023-11-25 23:40:13 +0900

    [Silver V] Title: 올림픽, Time: 52 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -9,11 +9,9 @@ for _ in range(N):
 
 medal.sort(key=lambda x: (x[1], x[2], x[3]), reverse=True)
 
-ans = 0
+idx = [medal[i][0] for i in range(N)].index(target)
 
-for k, v in enumerate(medal):
-    if v[0] == target:
-        ans = k
+for i in range(N):
+    if medal[idx][1:] == medal[i][1:]:
+        print(i + 1)
         break
-
-print(ans)
