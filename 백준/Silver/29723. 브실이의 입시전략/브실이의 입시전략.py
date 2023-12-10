d4138cd36ed8542b6afe6f244530dae5b27ed2fc
이정호 <101508190+jeongho1209@users.noreply.github.com>
2023-12-10 22:39:07 +0900

    [Silver V] Title: 브실이의 입시전략, Time: 52 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/29723. 브실이의 입시전략/브실이의 입시전략.py
+++ 백준/Silver/29723. 브실이의 입시전략/브실이의 입시전략.py
@@ -1,19 +1,26 @@
-N, M, K = map(int, input().split())
-subject = {}
+import sys
+
+N, M, K = map(int, sys.stdin.readline().rstrip().split())
+
+scores = {}
+
+ans = 0
+
 for _ in range(N):
-    s, p = input().split()
-    subject[s] = int(p)
+    name, score = sys.stdin.readline().rstrip().split()
+    scores[name] = int(score)
 
-open_subject = 0  # 브실대학에서 공개한 과목 점수 합
 for _ in range(K):
-    t = input()
-    open_subject += subject[t]
-    del subject[t]
+    target = sys.stdin.readline().rstrip()
+    ans += scores[target]
+    del scores[target]
 
-subject = sorted(subject.items(), key=lambda x: x[1])
-min_score, max_score = 0, 0  # 나머지 과목 최소, 최대 점수
+max_ans, min_ans = ans, ans
+
+scores = sorted(scores.items(), key=lambda x: x[1])
+
 for i in range(M - K):
-    min_score += subject[i][1]
-    max_score += subject[-i - 1][1]
+    min_ans += scores[i][1]
+    max_ans += scores[-i - 1][1]
 
-print(open_subject + min_score, open_subject + max_score)
+print(min_ans, max_ans)
