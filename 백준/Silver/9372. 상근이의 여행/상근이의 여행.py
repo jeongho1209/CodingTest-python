7e4bb9d4aa557a89e599c4b7b783b9ec99d6c445
이정호 <101508190+jeongho1209@users.noreply.github.com>
2024-01-02 11:07:06 +0900

    [Silver IV] Title: 상근이의 여행, Time: 160 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -5,11 +5,7 @@ t = int(sys.stdin.readline().rstrip())
 for _ in range(t):
     node, edge = map(int, sys.stdin.readline().rstrip().split())
 
-    trees = [[] for _ in range(node + 1)]
-
     for _ in range(edge):
-        src, dest = map(int, sys.stdin.readline().rstrip().split())
-        trees[src].append(dest)
-        trees[dest].append(src)
+        _, _ = map(int, sys.stdin.readline().rstrip().split())
 
     print(node - 1)
