c5bde2893c586f3b94b0a4aa4fd5950eb58ef6a0
이정호 <101508190+jeongho1209@users.noreply.github.com>
2024-01-03 16:36:13 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 5, Time: 588 ms, Memory: 74236 KB -BaekjoonHub

---
+++ 백준/Silver/24483. 알고리즘 수업 － 깊이 우선 탐색 5/알고리즘 수업 － 깊이 우선 탐색 5.py
@@ -0,0 +1,36 @@
+import sys
+
+sys.setrecursionlimit(10 ** 7)
+node, edge, R = map(int, sys.stdin.readline().rstrip().split())
+graph = [[] for _ in range(node + 1)]
+visited1 = [-1] * (node + 1)
+visited2 = [0] * (node + 1)
+cnt = 1
+
+for _ in range(edge):
+    src, dest = map(int, sys.stdin.readline().rstrip().split())
+
+    graph[src].append(dest)
+    graph[dest].append(src)
+
+
+def dfs(t, depth):
+    global cnt
+    visited1[t] = depth
+    visited2[t] = cnt
+    graph[t].sort()
+
+    for i in graph[t]:
+        if visited1[i] == -1 and visited2[i] == 0:
+            cnt += 1
+            dfs(i, depth + 1)
+
+
+dfs(R, 0)
+
+answer = 0
+
+for v1, v2 in zip(visited1[1:], visited2[1:]):
+    answer += v1 * v2
+
+print(answer)
