e1254c88248248749df3c2a2bf988603443544a4
이정호 <101508190+jeongho1209@users.noreply.github.com>
2024-01-04 14:19:46 +0900

    [Silver I] Title: 점프왕 쩰리 (Large), Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/16174. 점프왕 쩰리 （Large）/점프왕 쩰리 （Large）.py
@@ -0,0 +1,25 @@
+N = int(input())
+
+graph = []
+visited = [[False] * N for _ in range(N)]
+
+for _ in range(N):
+    graph.append(list(map(int, input().split())))
+
+
+def dfs(i, j):
+    if i >= N or j >= N:
+        return 0
+
+    if not visited[i][j]:
+        visited[i][j] = True
+        dfs(i + graph[i][j], j)
+        dfs(i, j + graph[i][j])
+
+
+dfs(0, 0)
+
+if visited[N - 1][N - 1]:
+    print("HaruHaru")
+else:
+    print("Hing")
