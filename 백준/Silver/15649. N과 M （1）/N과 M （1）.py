544d935b38110bbf64800e1f75d67871ce67c269
이정호 <101508190+jeongho1209@users.noreply.github.com>
2024-01-05 22:05:21 +0900

    [Silver III] Title: N과 M (1), Time: 168 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,20 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().rstrip().split())
+
+ans = []
+
+
+def dfs():
+    if len(ans) == M:
+        print(' '.join(map(str, ans)))
+        return
+
+    for i in range(1, N + 1):
+        if i not in ans:
+            ans.append(i)
+            dfs()
+            ans.pop()
+
+
+dfs()
