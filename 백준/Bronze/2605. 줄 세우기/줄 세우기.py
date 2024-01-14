73ed51bb7c1ef89287f7e98ea48e5ab997781457
이정호 <101508190+jeongho1209@users.noreply.github.com>
2024-01-14 12:37:21 +0900

    [Bronze II] Title: 줄 세우기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2605. 줄 세우기/줄 세우기.py
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.py
@@ -6,12 +6,7 @@ numbers = list(map(int, sys.stdin.readline().rstrip().split()))
 
 stack = [1]
 
-if numbers[1] == 0:
-    stack.append(2)
-else:
-    stack.insert(0, 2)
-
-for i in range(2, n):
+for i in range(1, n):
     stack.insert(i - numbers[i], i + 1)
 
-print(' '.join(map(str, stack)))
+print(*stack)
