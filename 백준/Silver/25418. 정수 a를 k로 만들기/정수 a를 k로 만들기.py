609612ad9d400b63c977b931eebb4144a6175c4b
이정호 <101508190+jeongho1209@users.noreply.github.com>
2024-02-11 21:05:07 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 768 ms, Memory: 102232 KB -BaekjoonHub

---
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.py
@@ -0,0 +1,25 @@
+import sys
+from collections import deque
+
+A, K = map(int, sys.stdin.readline().rstrip().split())
+
+queue = deque([(A, 0)])
+
+set1 = set()
+
+target = 0
+cnt = 0
+
+while queue:
+    v, c = queue.popleft()
+    if v == K:
+        print(c)
+        break
+
+    if v * 2 <= K and v * 2 not in set1:
+        set1.add(v * 2)
+        queue.append([v * 2, c + 1])
+
+    if v + 1 <= K and v + 1 not in set1:
+        set1.add(v + 1)
+        queue.append([v + 1, c + 1])
