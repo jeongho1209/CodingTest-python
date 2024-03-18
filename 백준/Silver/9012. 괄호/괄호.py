e07e3b59efdaae5ad5bb7f3b48e06e8c5b49d6e3
이정호 <101508190+jeongho1209@users.noreply.github.com>
2024-03-18 11:52:15 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,16 +1,25 @@
-for a in range(int(input())):
+import sys
+
+T = int(sys.stdin.readline().rstrip())
+
+for _ in range(T):
+    targets = list(sys.stdin.readline().rstrip())
+
     stack = []
-    for i in input():
-        if i == '(':
-            stack.append(i) # '('면 그냥 더하기
-        else:
-            if stack: # ')' 일때 짝 맞으니까 원래 있던 '(' 빼기
-                stack.pop()
+
+    ans = 'YES'
+
+    for t in targets:
+        if t == '(':
+            stack.append(t)
+        elif t == ')':
+            if stack:
+                if stack.pop() != '(':
+                    ans = 'NO'
             else:
-                print('NO') # ')' 이 처음으로 들어오면 잘못된 괄호
-                break # 어차피 아니니까 그냥 멈추기
-    else:
-        if not stack: # 다 비워졌으면 올바른 괄호
-            print('YES')
-        else: # '(' 괄호가 남아있으면 잘못된 괄호
-            print('NO')
+                ans = 'NO'
+
+    if stack:
+        ans = 'NO'
+
+    print(ans)
