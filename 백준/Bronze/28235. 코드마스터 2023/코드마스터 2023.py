2db8dc734517e7f01384964a458c18bf2dffbf65
이정호 <101508190+jeongho1209@users.noreply.github.com>
2024-05-05 18:46:07 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,12 @@
+import sys
+
+s = sys.stdin.readline().rstrip()
+
+if s == 'SONGDO':
+    print('HIGHSCHOOL')
+elif s == 'CODE':
+    print('MASTER')
+elif s == '2023':
+    print('0611')
+else:
+    print('CONTEST')
