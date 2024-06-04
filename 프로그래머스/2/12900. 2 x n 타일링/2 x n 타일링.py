6adbe0641089e706528dcdf0ff2d7bfe64e1a833
이정호 <101508190+jeongho1209@users.noreply.github.com>
2024-06-04 16:23:19 +0900

    [level 2] Title: 2 x n 타일링, Time: 12.98 ms, Memory: 13 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -0,0 +1,9 @@
+def solution(n):
+    dp = [0 for i in range(n)]
+    dp[0] = 1
+    dp[1] = 2
+    
+    for i in range(2, n):
+        dp[i] = (dp[i - 1] + dp[i - 2] % 1000000007)
+    
+    return dp[n - 1] % 1000000007
